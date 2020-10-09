55465e030d90db6c4120a0316183a24e32c68d5f
penpen09 <ar-el.mbl.10@outlook.jp>
2020-10-09 22:16:00 +0900

    add:events_controller

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,9 @@
 module EventsHelper
+  def choose_new_or_edit
+    if action_name == 'new' || action_name == 'create'
+      confirm_events_path
+    elsif action_name == 'edit'
+      event_path
+    end
+  end
 end
