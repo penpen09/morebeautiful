38a0465a03d84a07fcc04a042db9e403358b021d
penpen09 <ar-el.mbl.10@outlook.jp>
2020-10-10 00:33:24 +0900

    add:画像アップロード

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
   validates :title, presence: true, length: { in: 1..255 }
   validates :content, presence: true, length: { in: 1..900 }
+  mount_uploader :image, ImageUploader
 end
