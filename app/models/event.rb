587c662b18851d78d38f5aa0a04f55bb17dfcda3
penpen09 <ar-el.mbl.10@outlook.jp>
2020-10-15 21:33:03 +0900

    add:feeを0以上の値にするバリデーションを追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ApplicationRecord
   validates :title, presence: true, length: { in: 1..255 }
   validates :content, presence: true, length: { in: 1..900 }
+  validates :fee, numericality: {greater_than: 0}
   validates :date_before_today, presence: true
   mount_uploader :image, ImageUploader
   belongs_to :user
