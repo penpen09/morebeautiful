267b4de89ee3e348a6448324d47f6e7db0cf2e00
penpen09 <ar-el.mbl.10@outlook.jp>
2020-10-15 12:37:24 +0900

    add:辞書機能、update:編集削除ボタンの表示非表示

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,6 @@ module Morebeautiful
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
   end
 end
