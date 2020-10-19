5bff4d32170258efe79cba1791b91557552d3185
penpen09 <ar-el.mbl.10@outlook.jp>
2020-10-19 18:40:07 +0900

    :add:イベントのmodelスペック

--- spec/factories/event.rb
+++ spec/factories/event.rb
@@ -1,15 +1,18 @@
 FactoryBot.define do
   factory :event do
-    name { 'user_1' }
-    email { 'user_1r@example.com' }
-    password { 'password1' }
-    password_confirmation { "password1" }
+    title { 'メイク' }
+    content { 'メイクコンテント' }
+    place { 'オフライン' }
+    fee { 1000 }
+    event_date { "2020-10-30 09:00:00" }
+    user_id { 1 }
   end
   factory :event2, class: Event do
-    name { 'admin' }
-    email { 'admin@example.com' }
-    password { 'passwordadmin' }
-    password_confirmation { "passwordadmin" }
-    admin { true }
+    title { 'メイク2' }
+    content { 'メイクコンテント2' }
+    place { '東京都内' }
+    fee { 0 }
+    event_date { "2020-11-30 09:00:00" }
+    user_id { 2 }
   end
 end
