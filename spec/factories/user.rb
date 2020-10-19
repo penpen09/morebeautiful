ba1b859a08547cb52597420aac8bc6f47a1a69b1
penpen09 <ar-el.mbl.10@outlook.jp>
2020-10-19 13:33:11 +0900

    add:userのmodelスペック

---
+++ spec/factories/user.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :user do
+    id { 1 }
+    name { 'user_1' }
+    email { 'user_1r@example.com' }
+    password { 'password1' }
+    password_confirmation { "password1" }
+  end
+  factory :admin, class: User do
+    id { 2 }
+    name { 'admin' }
+    email { 'admin@example.com' }
+    password { 'passwordadmin' }
+    password_confirmation { "passwordadmin" }
+    admin { true }
+  end
+end
