5ffaeebba20823d11409557e930bbc84247ce435
penpen09 <ar-el.mbl.10@outlook.jp>
2020-10-21 12:33:48 +0900

    add:event_spec fix:ビュー

--- spec/factories/event.rb
+++ spec/factories/event.rb
@@ -5,7 +5,6 @@ FactoryBot.define do
     place { 'オフライン' }
     fee { 1000 }
     event_date { "2020-10-30 09:00:00" }
-    user_id { 1 }
   end
   factory :event2, class: Event do
     title { 'メイク2' }
@@ -13,6 +12,12 @@ FactoryBot.define do
     place { '東京都内' }
     fee { 0 }
     event_date { "2020-11-30 09:00:00" }
-    user_id { 2 }
+  end
+  factory :event3, class: Event do
+    title { 'メイク3' }
+    content { 'メイクコンテント3' }
+    place { '東京都内' }
+    fee { 0 }
+    event_date { "2020-01-30 09:00:00" }
   end
 end
