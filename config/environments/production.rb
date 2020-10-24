a9e046c7a33dc82ac5b1be8c38b0794f60ad2e3e
penpen09 <ar-el.mbl.10@outlook.jp>
2020-10-24 13:36:04 +0900

    add:sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,19 @@ Rails.application.configure do
   #   enable_starttls_auto: true,
   #   authentication: :plain
   #   }
-  # config.action_mailer.default_url_options = { host: 'murmuring-wave-61559.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'murmuring-wave-61559.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+  
   # config.action_mailer.delivery_method = :letter_opener_web
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
