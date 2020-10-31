f8d246c68d75262939f1a9978063d67c43bfceb0
penpen09 <ar-el.mbl.10@outlook.jp>
2020-11-01 02:22:02 +0900

    add:model/アソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,9 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorite_events, through: :favorites, source: :event
 
+  has_many :likes, dependent: :destroy
+  has_many :liked_posts, through: :likes, source: :post
+
   has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
   has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :follower
