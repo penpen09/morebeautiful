fe4ff762b5e2cfee13ff9cd47f9b40e4ab22ad2b
penpen09 <ar-el.mbl.10@outlook.jp>
2020-11-01 02:18:12 +0900

    add:likeモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_070011) do
+ActiveRecord::Schema.define(version: 2020_10_31_171705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,6 +98,15 @@ ActiveRecord::Schema.define(version: 2020_10_29_070011) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.bigint "post_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "messages", force: :cascade do |t|
     t.text "body"
     t.bigint "conversation_id"
@@ -182,6 +191,8 @@ ActiveRecord::Schema.define(version: 2020_10_29_070011) do
   add_foreign_key "events", "users"
   add_foreign_key "labelings", "events"
   add_foreign_key "labelings", "labels"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "messages", "conversations"
   add_foreign_key "messages", "users"
   add_foreign_key "postlabelings", "labels"
