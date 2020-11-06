17750038adfabb12bcf1d2552e79b1769c205079
penpen09 <ar-el.mbl.10@outlook.jp>
2020-11-06 23:02:47 +0900

    add:通知機能/model

--- app/models/event.rb
+++ app/models/event.rb
@@ -12,6 +12,7 @@ class Event < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :labelings, dependent: :destroy
   has_many :labels, through: :labelings, source: :label
+  has_many :notifications, dependent: :destroy
 
   def date_before_today
     errors.add(:event_date, "は本日以降のものを選択してください") if event_date.nil? || event_date < DateTime.now
@@ -32,8 +33,50 @@ class Event < ApplicationRecord
     end
   end
 
-
   def fee_validate
     errors.add(:fee, "は0より大きくなければなりません。") if fee.nil? || fee < 0
+  end
+
+  def create_notification_like!(current_user)
+    # すでに「いいね」されているか検索
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and post_id = ? and action = ? ", current_user.id, user_id, id, 'eventroom'])
+    # いいねされていない場合のみ、通知レコードを作成
+    if temp.blank?
+      notification = current_user.active_notifications.new(
+        event_id: id,
+        visited_id: user_id,
+        action: 'like'
+      )
+      # 自分の投稿に対するいいねの場合は、通知済みとする
+      if notification.visitor_id == notification.visited_id
+        notification.checked = true
+      end
+      notification.save if notification.valid?
+    end
+  end
+
+  def create_notification_comment!(current_user, comment_id)
+    # 自分以外にコメントしている人をすべて取得し、全員に通知を送る
+    temp_ids = Comment.select(:user_id).where(event_id: id).where.not(user_id: current_user.id).distinct
+    temp_ids.each do |temp_id|
+      save_notification_comment!(current_user, comment_id, temp_id['user_id'])
+    end
+    # まだ誰もコメントしていない場合は、投稿者に通知を送る
+    save_notification_comment!(current_user, comment_id, user_id) if temp_ids.blank?
+  end
+
+  def save_notification_comment!(current_user, comment_id, visited_id)
+    # コメントは複数回することが考えられるため、１つの投稿に複数回通知する
+    notification = current_user.active_notifications.new(
+      event_id: id,
+      comment_id: comment_id,
+      visited_id: visited_id,
+      action: 'comment'
+    )
+    # 自分の投稿に対するコメントの場合は、通知済みとする
+    if notification.visitor_id == notification.visited_id
+      notification.checked = true
+    end
+    notification.save if notification.valid?
   end
 end
---
+++ app/models/notification.rb
@@ -0,0 +1,6 @@
+class Notification < ApplicationRecord
+  belongs_to :event, optional: true
+  belongs_to :comment, optional: true
+  belongs_to :visitor, class_name: 'User', foreign_key: 'visitor_id', optional: true
+  belongs_to :visited, class_name: 'User', foreign_key: 'visited_id', optional: true
+end
